c9cf22c2bed8c902ac6a46039bb87aa769b2b5c3
Gil <ghopecs@gmail.com>
2023-11-20 23:41:22 -0500

    update

--- blogUtils.js
+++ blogUtils.js
@@ -1,17 +1,17 @@
-// // blogUtils.js
+// blogUtils.js
 
-// import fs from 'fs';
-// import path from 'path';
-// import fetch from 'node-fetch';
+import fs from 'fs';
+import path from 'path';
+import fetch from 'node-fetch';
 
-// // Function to get all markdown files from the directory
-// export function getMarkdownFiles(dir) {
-//     const files = fs.readdirSync(dir);
-//     return files.filter(file => file.endsWith('.md'));
-// }
+// Function to get all markdown files from the directory
+export function getMarkdownFiles(dir) {
+    const files = fs.readdirSync(dir);
+    return files.filter(file => file.endsWith('.md'));
+}
 
-// // Function to check if an article already exists on Dev.to
-// export async function checkIfArticleExists(title, apiKey) {
-//     // ... implementation to check if article exists ...
-//     // Use fetch with apiKey to call Dev.to API and compare titles
-// }
+// Function to check if an article already exists on Dev.to
+export async function checkIfArticleExists(title, apiKey) {
+    // ... implementation to check if article exists ...
+    // Use fetch with apiKey to call Dev.to API and compare titles
+}
