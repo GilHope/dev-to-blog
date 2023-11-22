c753ef51114d4e7dc718a8b2f470d4f3d094da80
Gil <ghopecs@gmail.com>
2023-11-22 13:13:54 -0500

    expand updateexistingarticle func

--- blogUpdater.js
+++ blogUpdater.js
@@ -4,12 +4,42 @@ import fs from 'fs';
 import path from 'path'; 
 import { getMarkdownFiles, checkIfArticleExists } from './blogUtils.js';
 import frontMatter from 'front-matter';
+import fetch from 'node-fetch';
 
 const DEV_TO_API_KEY = process.env.DEVTO_API_KEY;
 const BLOGS_DIR = './blogs';
 
 async function updateExistingArticle(articleId, title, content) {
-    // ... implementation of updating an existing article on Dev.to ...
+    console.log(`Attempting to update article: ${title} with ID: ${articleId}`);
+    const url = `https://dev.to/api/articles/${articleId}`;
+
+    try {
+        const response = await fetch(url, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'api-key': DEV_TO_API_KEY
+            },
+            body: JSON.stringify({ 
+                article: { 
+                    title, 
+                    body_markdown: content,
+                    published: true
+                } 
+            })
+        });
+
+        const data = await response.json();
+        console.log('API Response:', data);
+
+        if (!response.ok) {
+            throw new Error(`API Request Failed: ${response.status} ${response.statusText}`);
+        }
+
+        console.log(`Article updated successfully: ${data.url}`);
+    } catch (error) {
+        console.error('Failed to update article:', error);
+    }
 }
 
 async function processUpdatedMarkdownFiles() {
